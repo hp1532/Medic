91101e24b74bfcd1fbfe41b3cf32d841a7d01d92
Hari S <66002396+hp1532@users.noreply.github.com>
2020-07-28 01:20:00 -0700

    medic

---
+++ index.php
@@ -0,0 +1,360 @@
+
+  <?php include 'header.php';?>
+   <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Intro -->
+    <section class="p-t-b-150">
+      <div class="container">
+        <div class="intro-main">
+          <div class="row"> 
+            
+            <!-- Intro Detail -->
+            <div class="col-md-8">
+              <div class="text-sec">
+                <h5>Health Check Up At Care And Cure</h5>
+                <p>Besides providing world class clinical lab services, Labaid Diagnostic Centre houses a pool of doctors of different medical specialty to serve the ailing patients as outpatients. They are all reputed and respected in their medical specialty for outstanding clinical management</p>
+                <ul class="row">
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                  <li class="col-sm-6">
+                    <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                    <p>Excepteur sint occaecat cupidatat non roident, 
+                      sunt in culpa qui officia deserunt mollit </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            
+            <!-- Intro Timing -->
+            <div class="col-md-4">
+              <div class="timing"> <i class="lnr lnr-clock"></i>
+                <ul>
+                  <li> Monday <span>8.00 - 16.00</span></li>
+                  <li> Tuesday <span>8.00 - 16.00</span></li>
+                  <li> Wednesday <span>8.00 - 16.00</span></li>
+                  <li> Thursday <span>8.00 - 16.00</span></li>
+                  <li> Friday <span>8.00 - 16.00</span></li>
+                  <li> Saturday <span>8.00 - 16.00</span></li>
+                  <li> Sunday <span>8.00 - 16.00</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  
+
+    
+    <!-- DOCTOR LIST -->
+    <section class="p-t-b-150">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div class="heading-block">
+          <h2>Our Services</h2>
+          <hr>
+          <span>Duis autem vel eum iriure dolor in hendrerit n vuew lputate velit esse molestie conseu vel illum dolore eufe ugiat nulla facilisis at vero.</span> </div>
+        
+        <!-- Services -->
+        <div class="services">
+          <div class="row"> 
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-eye-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Eye Specialist</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-operating-room icon"></i> </div>
+                <div class="media-body">
+                  <h6>Operation Theater</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-icu-monitor icon"></i> </div>
+                <div class="media-body">
+                  <h6>ICU Department</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-doctor icon"></i> </div>
+                <div class="media-body">
+                  <h6>Qualified Doctors</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-heartbeat icon"></i> </div>
+                <div class="media-body">
+                  <h6>Heart Problems</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+            
+            <!-- Services -->
+            <div class="col-md-4">
+              <article>
+                <div class="media-left"> <i class="flaticon-stomach-2 icon"></i> </div>
+                <div class="media-body">
+                  <h6>Stomach Problems</h6>
+                  <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit random text.</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    
+    <!-- Departments -->
+    <section class="p-t-b-150 padding-bottom-0">
+      <div class="container"> 
+        
+        <!-- Heading -->
+        <div class="heading-block">
+          <h2>Our Best Departments</h2>
+          <hr>
+      </div>
+      
+      <!-- Department -->
+      <div class="department">
+        <div class="container"> 
+          <!-- Tab Panel -->
+          <div role="tabpanel">
+            <div class="dep-sec-nav"> 
+              <!-- Nav tabs -->
+              <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation"><a href="#dental" role="tab" data-toggle="tab">Dental</a></li>
+                <li role="presentation" class="active"><a href="#cardiology" role="tab" data-toggle="tab">Cardiology </a></li>
+                <li role="presentation"><a href="#for-disabled" role="tab" data-toggle="tab">For disabled </a></li>
+                <li role="presentation"><a href="#ophthalmology" role="tab" data-toggle="tab">Ophthalmology </a></li>
+                <li role="presentation"><a href="#emergency" role="tab" data-toggle="tab"> Emergency </a></li>
+              </ul>
+            </div>
+            
+            <!-- Tab Content -->
+            <div class="tab-content"> 
+              
+              <!-- Dental Depatment -->
+              <div role="tabpanel" class="tab-pane fade" id="dental"> 
+                <!-- Depatment Text Section -->
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best <span>Dental</span> Services</h5>
+                      <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, <br>
+                        <br>
+                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s PageMaker </p>
+                      
+                      <!-- List Style Check -->
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img -->
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="images/depart-doctor-img-3.jpg" alt="" >
+                        <div class="name-doc">
+                          <h6>Prof. S. M. MUSTAFA ZAMAN <span>Cardiology Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- Cardiology Depatment -->
+              <div role="tabpanel" class="tab-pane fade in active" id="cardiology"> 
+                <!-- Depatment Text Section -->
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best <span>CARDIOLOGY</span> Services</h5>
+                      <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, <br>
+                        <br>
+                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s PageMaker including versions of Lorem Ipsum. </p>
+                      
+                      <!-- List Style Check -->
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img -->
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="images/depart-doctor-img-1.jpg" alt="" >
+                        <div class="name-doc">
+                          <h6>Prof. Dr. Asia Khanam <span>Cardiology Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- For disabled -->
+              <div role="tabpanel" class="tab-pane fade" id="for-disabled"> 
+                <!-- Depatment Text Section -->
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best Services <span>For Disabled</span> Persons </h5>
+                      <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, <br>
+                        <br>
+                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s PageMaker including versions of Lorem Ipsum. </p>
+                      
+                      <!-- List Style Check -->
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img -->
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="images/depart-doctor-img-2.jpg" alt="" >
+                        <div class="name-doc">
+                          <h6>Asso. Prof. Dr. Indrajit Prashad<span>Cardiology Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- Ophthalmology -->
+              <div role="tabpanel" class="tab-pane fade" id="ophthalmology"> 
+                <!-- Depatment Text Section -->
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best <span>Ophthalmology</span> Services</h5>
+                      <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, <br>
+                        <br>
+                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s PageMaker including versions of Lorem Ipsum. </p>
+                      
+                      <!-- List Style Check -->
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img -->
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="images/depart-doctor-img.jpg" alt="" >
+                        <div class="name-doc">
+                          <h6>Smith Jhon <span>Ophthalmology Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              
+              <!-- emergency -->
+              <div role="tabpanel" class="tab-pane fade" id="emergency"> 
+                <!-- Depatment Text Section -->
+                <div class="dep-sec-txt">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <h5>Our Best <span>Emergency</span> Services</h5>
+                      <p> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, <br>
+                        <br>
+                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s PageMaker including versions of Lorem Ipsum. </p>
+                      
+                      <!-- List Style Check -->
+                      <ul class="list-style-check">
+                        <li> Qualified Staff of Doctors </li>
+                        <li> Feel like you are at Home Services </li>
+                        <li> 24x7 Emergency Services </li>
+                        <li> Save your Money and Time with us </li>
+                        <li> Medicine Research </li>
+                        <li> Blood Bank </li>
+                      </ul>
+                    </div>
+                    
+                    <!-- Doctor Img -->
+                    <div class="col-md-offset-1 col-md-4">
+                      <div class="doctor-img"> <img class="img-responsive" src="images/depart-doctor-img.jpg" alt="" >
+                        <div class="name-doc">
+                          <h6>Smith Jhon <span>Emergency Specialists</span></h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+   
+    
+    
+  </div>
+  
+  <!-- Footer -->
+<?php include 'footer.php';?>
